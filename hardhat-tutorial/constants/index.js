feba4e8166576ced5a1c1daef8e7c05dfd16bffb
AzureKn1ght <philsonnah@msn.com>
2022-08-14 18:16:25 +0800

    Added Smart Contracts

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xf51CCB5D3B54a5b4d08121945A6D64D9524894b9";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
